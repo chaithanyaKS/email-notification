a6378506efef10063dcb57f7e5a86c5693f1c10d
chaithanya2934 <chaithanya2934@gmail.com>
2022-01-14 23:36:46 +0530

    fixed prop issue

--- components/MarkDownPreview/MarkDownPreview.tsx
+++ components/MarkDownPreview/MarkDownPreview.tsx
@@ -11,7 +11,7 @@ const Preview = ({ data }: MarkDownPreviewProps) => {
       <div className="p-4 bg-white rounded-md flex-grow shadow-md overscroll-x-scroll">
         <h2 className="text-2xl font-bold">Body</h2>
         <hr className="my-2 border-t-[3px] rounded-full" />
-        <ReactMarkdown className="break-words" children={data.body} />
+        <ReactMarkdown className="break-words">{data.body}</ReactMarkdown>
       </div>
     </>
   )
